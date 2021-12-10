39156eaa36a6a0b3e802727c9bb515d9db2d7ebb
Federico Barrios Meneses <federico.barrios@masiv.com>
2021-12-09 22:40:41 -0500

    Product by id

--- src/main/java/org/venda/pues/reportapi/controller/ReportingController.java
+++ src/main/java/org/venda/pues/reportapi/controller/ReportingController.java
@@ -1,15 +1,12 @@
 package org.venda.pues.reportapi.controller;
 
-import models.ProductDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.venda.pues.reportapi.request.ReportRequest;
 import org.venda.pues.reportapi.service.ReportingServices;
 
-import java.util.List;
-
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/v1/report")
 public class ReportingController {
 
@@ -19,19 +16,13 @@ public class ReportingController {
         this.reportingServices = reportingServices;
     }
 
-    @GetMapping("/sold-out")
-    public ResponseEntity<?> getSoldOutProducts() {
-        List<ProductDocument> result = reportingServices.getSoldOutProducts();
-        return ResponseEntity.ok(result);
+    @GetMapping("/{userId}/sold-out")
+    public ResponseEntity<?> getSoldOutProducts(@PathVariable String userId) {
+        return ResponseEntity.ok(reportingServices.getSoldOutProducts(userId));
     }
 
-    @PostMapping("/products-popularity")
-    public ResponseEntity<?> getProductsPopularity(@RequestBody ReportRequest reportRequest) {
-        return ResponseEntity.ok(this.reportingServices.getProductsPopularity(reportRequest.getInitDate(), reportRequest.getEndDate()));
-    }
-
-    @PostMapping("/sales")
-    public ResponseEntity<?> getSales(@RequestBody ReportRequest reportRequest) {
-        return ResponseEntity.ok(reportingServices.getSalesPerTimePeriod(reportRequest.getInitDate(), reportRequest.getEndDate()));
+    @GetMapping("/{userId}/sales")
+    public ResponseEntity<?> getSales(@PathVariable String userId) {
+        return ResponseEntity.ok(reportingServices.getSalesPerTimePeriod(userId));
     }
 }
--- src/main/java/org/venda/pues/reportapi/service/ReportingServices.java
+++ src/main/java/org/venda/pues/reportapi/service/ReportingServices.java
@@ -1,15 +1,17 @@
 package org.venda.pues.reportapi.service;
 
+import dto.ProductSaleDetailsDto;
+import error.exception.NotFoundException;
 import models.ProductDocument;
 import models.SaleDocument;
+import models.UserDocument;
 import org.springframework.stereotype.Service;
 import org.venda.pues.reportapi.model.SalesSummary;
 import org.venda.pues.reportapi.repository.ProductRepository;
 import org.venda.pues.reportapi.repository.SaleRepository;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.venda.pues.reportapi.repository.UserRepository;
 
-import java.util.Date;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,37 +19,43 @@ public class ReportingServices {
 
     private final ProductRepository productRepository;
     private final SaleRepository saleRepository;
+    private final UserRepository userRepository;
 
-    public ReportingServices(ProductRepository productRepository, SaleRepository saleRepository) {
+    public ReportingServices(ProductRepository productRepository, SaleRepository saleRepository, UserRepository userRepository) {
         this.productRepository = productRepository;
         this.saleRepository = saleRepository;
+        this.userRepository = userRepository;
     }
 
-    public List<ProductDocument> getSoldOutProducts() {
-        return productRepository.findByStockEquals(0);
-    }
-
-    public HashMap<String, Integer> getProductsPopularity(Date initDate, Date endDate) {
-        HashMap<String, Integer> salesByProduct = new HashMap<>();
-        for (SaleDocument sale: saleRepository.findBySoldAtBetween(initDate, endDate)) {
-            for (String product: sale.getProducts()) {
-                if (salesByProduct.containsKey(product)) {
-                    salesByProduct.put(product, salesByProduct.get(product) + 1);
-                } else {
-                    salesByProduct.put(product, 0);
+    public List<ProductDocument> getSoldOutProducts(String userId) {
+        UserDocument user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            List<ProductDocument> products = new ArrayList<>();
+            for (String productId : user.getProducts()) {
+                ProductDocument product = productRepository.findByIdEqualsAndStockEquals(productId, 0);
+                if (product != null) {
+                    products.add(productRepository.findByIdEqualsAndStockEquals(productId, 0));
                 }
             }
-        }
 
-        return salesByProduct;
+            return products;
+        }
+        throw new NotFoundException("User not found");
     }
 
-    public SalesSummary getSalesPerTimePeriod(Date initDate, Date endDate) {
-        List<SaleDocument> sales = saleRepository.findBySoldAtBetween(initDate, endDate);
-        Double totalSalesAmount = totalSalesAmountCalculator(sales);
-        Double totalSalesCost = totalSalesCostCalculator(sales);
+    public SalesSummary getSalesPerTimePeriod(String userId) {
+        UserDocument user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            List<SaleDocument> sales = new ArrayList<>();
+            for (String saleId: user.getSales()) {
+                saleRepository.findById(saleId).ifPresent(sales::add);
+            }
+            Double totalSalesAmount = totalSalesAmountCalculator(sales);
+            Double totalSalesCost = totalSalesCostCalculator(sales);
 
-        return new SalesSummary(totalSalesAmount, sales.size(), totalSalesCost, totalSalesAmount - totalSalesCost);
+            return new SalesSummary(totalSalesAmount, sales.size(), totalSalesCost, totalSalesAmount - totalSalesCost);
+        }
+        throw new NotFoundException("User not found");
     }
 
     private Double totalSalesAmountCalculator(List<SaleDocument> sales) {
@@ -61,18 +69,12 @@ public class ReportingServices {
 
     private Double totalSalesCostCalculator(List<SaleDocument> sales) {
         double aggregate = 0;
-        for (SaleDocument sale: sales) {
-            aggregate = totalProductsCostCalculator(sale.getProducts(), aggregate);
-        }
-
-        return aggregate;
-    }
-
-    private Double totalProductsCostCalculator(List<String> products, Double aggregate) {
-        for (String product: products) {
-            ProductDocument productDocument = productRepository.findById(product).orElse(null);
-            if(productDocument != null) {
-                aggregate += productDocument.getPrice();
+        for (SaleDocument sale : sales) {
+            for (ProductSaleDetailsDto saleDetails : sale.getProducts()) {
+                ProductDocument product = productRepository.findById(saleDetails.getProductId()).orElse(null);
+                if (product != null) {
+                    aggregate += product.getPrice() * saleDetails.getQuantity();
+                }
             }
         }
 
